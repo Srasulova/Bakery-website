b2da903f0b1e94a672ab1e123f0ed4035b88175a
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-08 16:46:17 -0400

    update page file

--- bakery-website/src/app/page.tsx
+++ bakery-website/src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import NavBar from "./navBar";
+import HeroAera from "./heroAera";
+import HowItWorks from "./howItworks";
+import Footer from "./footer";
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
+      <HeroAera />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 }
