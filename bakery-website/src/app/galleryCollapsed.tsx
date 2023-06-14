a2979a56a4786e66913a3b709269caacfcd46c8c
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-13 23:15:26 -0400

    add interactivity with next and previous buttons

--- bakery-website/src/app/galleryCollapsed.tsx
+++ bakery-website/src/app/galleryCollapsed.tsx
@@ -4,7 +4,29 @@ import "./galleryCollapsed.css";
 import galleryPictures from "./galleryImages";
 
 export default function GalleryCollapsed() {
-  const rightSideGridImages = galleryPictures.slice(0, 9);
+  const rightSideGridImages: any = galleryPictures.slice(0, 9);
+  const [currentIndex, setCurrentIndex]: [
+    number,
+    React.Dispatch<React.SetStateAction<number>>
+  ] = useState(0);
+
+  const mainImage = galleryPictures[currentIndex];
+
+  const showNextImage = () => {
+    const nextImage =
+      currentIndex === galleryPictures.length - 1 ? 0 : currentIndex + 1;
+
+    // const nextImage = (currentIndex + 1) % galleryPictures.length;
+    setCurrentIndex(nextImage);
+  };
+
+  const showPreviousImage = () => {
+    const previousImage =
+      currentIndex === 0 ? galleryPictures.length - 1 : currentIndex - 1;
+    // const previousImage =
+    //   (currentIndex - 1 + galleryPictures.length) % galleryPictures.length;
+    setCurrentIndex(previousImage);
+  };
 
   return (
     <>
@@ -17,17 +39,33 @@ export default function GalleryCollapsed() {
 
         <div className="galleryContainer container-fluid container">
           <div className="row justify-content-center">
-            <div className="col-md-6 gallery-img">
+            <div className="col-md-6 gallery-img main-img">
               <Image
                 className="img-fluid rounded"
-                src={galleryPictures[10].src}
-                alt={galleryPictures[10].alt}
+                src={mainImage.src}
+                alt={mainImage.alt}
               />
+              <div className="arrows-div d-flex justify-content-between mx-3">
+                <i
+                  className="bi bi-arrow-left-circle-fill"
+                  style={{ fontSize: "3rem", opacity: 0.5, color: "white" }}
+                  onClick={showPreviousImage}
+                ></i>
+                <i
+                  className="bi bi-arrow-right-circle-fill"
+                  style={{ fontSize: "3rem", opacity: 0.5, color: "white" }}
+                  onClick={showNextImage}
+                ></i>
+              </div>
             </div>
             <div className="col-md-6 gallery-img container">
               <div className="row row-cols-3 g-2">
                 {rightSideGridImages.map((image: any, index: number) => (
-                  <div className="col" key={image.id}>
+                  <div
+                    className="col"
+                    key={image.id}
+                    onClick={() => setCurrentIndex(index)}
+                  >
                     <Image
                       src={image.src}
                       alt={image.alt}
