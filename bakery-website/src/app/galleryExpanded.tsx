bf71691f99a0789330aec70ba0bde10a8f2e3cef
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-14 01:16:00 -0400

    add galleryExpanded page

---
+++ bakery-website/src/app/galleryExpanded.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { useState } from "react";
+import "./galleryExpanded.css";
+import galleryPictures from "./galleryImages";
+
+export default function GalleryExpanded() {
+  return (
+    <div className="gallery">
+      <div className="gallery-title" id="gallery-title">
+        <h2 className="gallery-title text-center fs-1 fw-bolder mt-5 mb-3 text-success">
+          Gallery
+        </h2>
+      </div>
+
+      <div className="gallery-container container">
+        <button
+          type="button"
+          className="btn btn-outline-success btn-less btn-sm mt-3 fw-medium mb-2"
+        >
+          Less {"<-"}
+        </button>
+        <div className="row row-cols-5 g-2">
+          {galleryPictures.map((image: any) => (
+            <div className="col" key={image.id}>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                className="img-fluid rounded gallery-img"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
