c409ae86fb7ec5597ac91ac2f653fa2bf6ef8435
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-16 01:38:37 -0400

    create a shopping cart modal

---
+++ bakery-website/src/app/shoppingCart.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import OpenOrderFormModal from "./orderForm";
+
+export default function ShoppingCart() {
+  return (
+    <>
+      <div
+        className="modal fade"
+        id="shoppingCartModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered text-success">
+          <div className="modal-content">
+            <div className="modal-header d-flex">
+              <h5
+                className="modal-title fs-5 text-center fw-bold text-success col-11"
+                id="exampleModalLabel"
+              >
+                Order details
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p className="firstName"></p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-success mx-auto">
+                Confirm your order
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <OpenOrderFormModal />
+    </>
+  );
+}
