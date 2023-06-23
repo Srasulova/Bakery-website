40c0c6e56dbbcab9613618f1d8f2b545d32dba7c
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-23 14:37:09 -0400

    delete comments

--- bakery-website/src/app/page.tsx
+++ bakery-website/src/app/page.tsx
@@ -30,9 +30,6 @@ export default function Home() {
         <GalleryCollapsed toggleGallery={handleToggleGallery} />
       )}
 
-      {/* <GalleryExpanded /> */}
-      {/* <GalleryCollapsed /> */}
-
       <Footer />
     </main>
   );
